35e2887b9bc55c9fe2b4151f2bd3150a9c096e0f
Kunszt Norbert <kunszt.norbert@gmail.com>
2023-05-12 17:59:26 +0200

    column alignment

--- src/ruanderGyakorlasFrammel/OsztalyProram.java
+++ src/ruanderGyakorlasFrammel/OsztalyProram.java
@@ -14,18 +14,21 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import javax.swing.JTable;
 import javax.swing.border.BevelBorder;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.JLabel;
+import java.awt.Font;
 
 public class OsztalyProram {
 	private JFrame frame;
 	private static List<Tanulo> tanuloLista = new ArrayList<Tanulo>();
-	
-private DefaultListModel<String> listaModell;
-private JList<String> listElemtanulokLista;
-String data[][];
 
-private JTable j;
-private JLabel lblOsztalyAtlag;
+	private DefaultListModel<String> listaModell;
+	private JList<String> listElemtanulokLista;
+	String data[][];
+
+	private JTable jtable;
+	private JLabel lblOsztalyAtlag;
+
 	/**
 	 * Launch the application.
 	 */
@@ -57,73 +60,71 @@ private JLabel lblOsztalyAtlag;
 		frame.addWindowListener(new WindowAdapter() {
 			@Override
 			public void windowClosing(WindowEvent e) {
-				Object[] opciok = {"Igen", "Nem"};
-				if (JOptionPane.showOptionDialog(frame, "Biztos ki akar lépni?", "Kilépés",
-						JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, opciok, opciok[1]) == JOptionPane.YES_OPTION) {
-					
+				Object[] opciok = { "Igen", "Nem" };
+				if (JOptionPane.showOptionDialog(frame, "Biztos ki akar lépni?", "Kilépés", JOptionPane.YES_NO_OPTION,
+						JOptionPane.QUESTION_MESSAGE, null, opciok, opciok[1]) == JOptionPane.YES_OPTION) {
+
 					System.exit(0);
-					
+
 				}
 			}
 		});
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		frame.setTitle( "Osztály");
-		
-		
-		
-		
+		frame.setTitle("Osztály");
+
 		FileHandling.readFile("tanulok.txt", ";", tanuloLista);
 
-	    lblOsztalyAtlag = new JLabel("");
-	    lblOsztalyAtlag.setBounds(10, 306, 386, 14);
-	   addElemToList();
-		
-	    
-	   String column[]={"Név","Angol","Töri","Matek","Átlag"};         
-	    
-	    
-	    j = new JTable(data, column);
-        j.setBounds(30, 40, 200, 300);
-        JScrollPane sp = new JScrollPane(j);
-        sp.setSize(604, 200);
-        sp.setLocation(20, 20);
-        frame.getContentPane().add(sp);
-        
-        
-        
-        
-        
-        
-	    frame.getContentPane().add(sp);          
-	    
-	    frame.getContentPane().add(lblOsztalyAtlag);
-	    
-	    
-	    frame.setSize(701,400);    
-	    frame.setVisible(true);
-		
+		lblOsztalyAtlag = new JLabel("");
+		lblOsztalyAtlag.setBounds(10, 306, 386, 14);
+		addElemToList();
+
+		String column[] = { "Név", "Angol", "Töri", "Matek", "Átlag" };
+
+		jtable = new JTable(data, column);
+		jtable.setFont(new Font("Tahoma", Font.ITALIC, 13));
+		jtable.setColumnSelectionAllowed(true);
+		jtable.setCellSelectionEnabled(true);
+		jtable.setBounds(30, 40, 200, 300);
+		setTableTextAlignment();
+		JScrollPane scroll = new JScrollPane(jtable);
+		scroll.setSize(604, 200);
+		scroll.setLocation(20, 20);
+		frame.getContentPane().add(scroll);
+		frame.getContentPane().add(scroll);
+		frame.getContentPane().add(lblOsztalyAtlag);
+		frame.setSize(701, 400);
+		frame.setVisible(true);
+
 	}
-	
-	private double sumClassAverage(double all) {		
-		return all/tanuloLista.size();
-		
+
+	private void setTableTextAlignment() {
+		DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
+		centerRenderer.setHorizontalAlignment(JLabel.CENTER);
+		for (int i = 1; i < 5; i++) {
+			jtable.getColumnModel().getColumn(i).setCellRenderer(centerRenderer);
+		}
+
 	}
-	
+
+	private double sumClassAverage(double all) {
+		return all / tanuloLista.size();
+
+	}
+
 	private void addElemToList() {
 		double atlag = 0;
 		data = new String[tanuloLista.size()][5];
-		for(int i=0;i<tanuloLista.size();i++) {
-			data[i][0]=tanuloLista.get(i).getNev();
-			data[i][1]=tanuloLista.get(i).getAngol()+"";
-			data[i][2]=tanuloLista.get(i).getTori()+"";
-			data[i][3]=tanuloLista.get(i).getMatek()+"";
-			data[i][4]=String.format("%.2f",tanuloLista.get(i).getAtlag());  
-			atlag+=tanuloLista.get(i).getAtlag();
+		for (int i = 0; i < tanuloLista.size(); i++) {
+			data[i][0] = tanuloLista.get(i).getNev();
+			data[i][1] = tanuloLista.get(i).getAngol() + "";
+			data[i][2] = tanuloLista.get(i).getTori() + "";
+			data[i][3] = tanuloLista.get(i).getMatek() + "";
+			data[i][4] = String.format("%.2f", tanuloLista.get(i).getAtlag());
+			atlag += tanuloLista.get(i).getAtlag();
 		}
-		lblOsztalyAtlag.setText("Osztályátlag: "+String.format("%.2f",sumClassAverage(atlag)));
-		
-		
+		lblOsztalyAtlag.setText("Osztályátlag: " + String.format("%.2f", sumClassAverage(atlag)));
+
 	}
 }
