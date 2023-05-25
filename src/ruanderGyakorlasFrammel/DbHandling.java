4d85a55b2e36cf7c4d475570d94670b0f735b5a9
Kunszt Norbert <kunszt.norbert@gmail.com>
2023-05-25 21:15:02 +0200

    beautify code

--- src/ruanderGyakorlasFrammel/DbHandling.java
+++ src/ruanderGyakorlasFrammel/DbHandling.java
@@ -10,42 +10,40 @@ import java.util.List;
 import javax.swing.JOptionPane;
 
 public class DbHandling {
-	private List<Tanulo> lista ;
-	private String dbName="felhasznalo_db";
-	private String dbUser="root";
-	private String dbPassword="";
-	private String dbPort="3306";
-	private String dbUrl="localhost";
-	
+	private List<Tanulo> lista;
+	private String dbName = "felhasznalo_db";
+	private String dbUser = "root";
+	private String dbPassword = "";
+	private String dbPort = "3306";
+	private String dbUrl = "localhost";
+
 	public DbHandling(List<Tanulo> lista) {
-		this.lista=lista;
+		this.lista = lista;
 	}
+
 	public void connect() {
-		try {
+		try(Connection con = DriverManager.getConnection("jdbc:mysql://" + dbUrl + ":" + dbPort + "/" + dbName + "",
+				dbUser, dbPassword)) {
 			Class.forName("com.mysql.jdbc.Driver");
-			Connection con = DriverManager.getConnection("jdbc:mysql://"+dbUrl+":"+dbPort+"/"+dbName+"", dbUser, dbPassword);
-			
-			//JOptionPane.showMessageDialog(null, "sikerült kapcsolódni");
-			filList(con, lista);
+			;
+
+			filList(con);
 			System.out.println(lista.size());
 		} catch (Exception e) {
 			System.out.println(e);
 			JOptionPane.showMessageDialog(null, "nem sikerült kapcsolódni");
 		}
 	}
-	
-	private void filList(Connection con, List<Tanulo> lista) {
+
+	private void filList(Connection con) {
 		Statement stmt;
 		try {
 			stmt = con.createStatement();
-		
-		ResultSet rs = stmt.executeQuery("select * from osztalyzatok");
-		while (rs.next()) {
-			
-			lista.add(new Tanulo(rs.getString("nev"),rs.getInt("matek"),rs.getInt("angol"),rs.getInt("tori")));
-		}
-			//System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3));
-		con.close();
+
+			ResultSet rs = stmt.executeQuery("select * from osztalyzatok");
+			while (rs.next()) {
+				lista.add(new Tanulo(rs.getString("nev"), rs.getInt("matek"), rs.getInt("angol"), rs.getInt("tori")));
+			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
